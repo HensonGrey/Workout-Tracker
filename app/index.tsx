81e5c7b960710dec2ab8c25e212b733b78af232f
unknown <valiomihailov.mih@gmail.com>
2024-12-23 15:13:58 +0200

    init

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="h-full bg-slate-600">
+      <Text>test</Text>
+    </SafeAreaView>
+  );
+}
