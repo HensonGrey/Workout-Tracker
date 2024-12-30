2ed3917b613aa1e786d4b7120eac13aa7f6e08f4
unknown <valiomihailov.mih@gmail.com>
2024-12-30 14:32:09 +0200

    home page and fixed tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
 
-export default function HomeScreen() {
+import "../global.css";
+
+export default function App() {
   return (
-    <SafeAreaView className="h-full bg-slate-600">
-      <Text>test</Text>
+    <SafeAreaView className="flex flex-1 flex-col bg-slate-500 h-full">
+      <View className="flex-[6]"></View>
+
+      <View className="flex-[1] flex flex-row">
+        <TouchableOpacity className="flex-[1] justify-center bg-slate-600 rounded-t-2xl border-r-2">
+          <Text className="text-center text-2xl color-white">program</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="flex-[1] justify-center bg-slate-600 rounded-t-2xl">
+          <Text className="text-center text-2xl color-white">start</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="flex-[1] justify-center bg-slate-600 rounded-t-2xl border-l-2">
+          <Text className="text-center text-2xl color-white">history</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
