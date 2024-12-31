bf1e1a89b8640655a78f854fd04d2c21a9a02369
unknown <valiomihailov.mih@gmail.com>
2024-12-31 17:13:28 +0200

    created some semblance of navigation that works so when i fuck up I can get back

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,16 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 import React from "react";
 import HomeScreen from "./index";
+import ProgramScreen from "./screens/ProgramScreen";
+import { StatusBar } from "expo-status-bar";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
 const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -31,16 +21,21 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "grey" },
+          headerTitleAlign: "center",
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Program" component={ProgramScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,13 +4,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../global.css";
 
-export default function App() {
+export default function HomeScreen({ navigation }: any) {
   return (
     <SafeAreaView className="flex flex-1 flex-col bg-slate-500 h-full">
       <View className="flex-[6]"></View>
 
       <View className="flex-[1] flex flex-row">
-        <TouchableOpacity className="flex-[1] justify-center bg-slate-600 rounded-t-2xl border-r-2">
+        <TouchableOpacity
+          className="flex-[1] justify-center bg-slate-600 rounded-t-2xl border-r-2"
+          onPress={() => navigation.navigate("Program")}
+        >
           <Text className="text-center text-2xl color-white">program</Text>
         </TouchableOpacity>
 
